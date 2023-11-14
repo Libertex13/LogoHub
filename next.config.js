ad40d381e56bbb1a22d6f39acb7f1cf7cfeb67bd
Libertex13 <nachoplanas.np@gmail.com>
2023-11-14 09:56:41 +0100

    Config

--- next.config.js
+++ next.config.js
@@ -11,6 +11,10 @@ const nextConfig = {
         protocol: 'https',
         hostname: 'unekyxkpgprmukgaidne.supabase.co'
       }
+    ],
+    domains: [
+      'oaidalleapiprodscus.blob.core.windows.net',
+      'unekyxkpgprmukgaidne.supabase.co'
     ]
   }
 };
